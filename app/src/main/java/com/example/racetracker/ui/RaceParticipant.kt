7fd05f6d98538a61c51d7d3a1a7434cbbe59977e
FunnyValentine <darkbrewx@gmail.com>
2024-08-18 20:22:17 +0900

    3. 添加一个可挂起的计算分数的 run 方法

--- app/src/main/java/com/example/racetracker/ui/RaceParticipant.kt
+++ app/src/main/java/com/example/racetracker/ui/RaceParticipant.kt
@@ -18,6 +18,7 @@ package com.example.racetracker.ui
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import kotlinx.coroutines.delay
 
 /**
  * This class represents a state holder for race participant.
@@ -39,6 +40,19 @@ class RaceParticipant(
      */
     var currentProgress by mutableStateOf(initialProgress)
         private set
+
+    // 使用suspend声明一个挂起的 run 函数
+    // 挂起: 简单来说就是可以暂停,且不会堵塞进程
+    suspend fun run() {
+        // 当当前进度小于 100,持续的增加进度值
+        // 示意: 主线程(while() -> delay() -> 其他任务(不会阻塞) -> currentProgress += progressIncrement)
+        while (currentProgress < maxProgress) {
+            // 使用 delay() 挂起函数, 模拟比赛中不同的进度间隔, 间隔时间使用progressDelayMillis的值
+            // <- 注意在 delay(progressDelayMillis) 左边的行数处有一个箭头的 icon, 表示这是一个挂起点, 函数可能会挂起,稍后再恢复运行
+            delay(progressDelayMillis)
+            currentProgress += progressIncrement
+        }
+    }
 
     /**
      * Regardless of the value of [initialProgress] the reset function will reset the
